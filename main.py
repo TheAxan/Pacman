b26932e20e6f60317f2051b8a286b3af1621146e
TheAxan <88592270+TheAxan@users.noreply.github.com>
2021-09-12 22:12:41 -0400

    Added pac and pac free movement. Also did re-formatting

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import pygame
 import sys
 
-white = 255, 255, 255
-black = 0, 0, 0
-grey = 100, 100, 100
-dark_grey = 15, 15, 15
-yellow = 255, 255, 0
-blue = 0, 0, 255
 
 pygame.init()
 
+# general definitions
 map_grid = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -44,32 +39,59 @@ map_grid = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-# map display
-
 size_mod = 3
 u = 10 * size_mod       # 1 unit is 10 pixels * the size modifier
+screen = pygame.display.set_mode((28 * u, 31 * u))
 
-screen = pygame.display.set_mode([28 * u, 31 * u])
+# colors definition
+white = 255, 255, 255
+black = 0, 0, 0
+dark_grey = 15, 15, 15
+yellow = 255, 255, 0
+blue = 0, 0, 255
 
-square = pygame.Surface((u, u))     # squares of 1 unit
+# background surface
+background = pygame.Surface((28 * u, 31 * u))
+
+square = pygame.Surface((u, u))     # Each cell is a squares of 1 unit
 square.fill(blue)
-pygame.draw.rect(square, dark_grey, (0, 0, u, u), 10)     # to identify separate squares
-
+pygame.draw.rect(square, dark_grey, (0, 0, u, u), 10)       # This is to identify separate squares visually
 
 y_counter = 0
 for row in map_grid:
-    x_counter = 0       # must be reset for each row
+    x_counter = 0
     for i in row:
         if i:
-            screen.blit(square, (x_counter * u, y_counter * u))     # draws map
+            background.blit(square, (x_counter * u, y_counter * u))     # This draws the map
         x_counter += 1
     y_counter += 1
 
-pygame.display.flip()
+# pac surface
+pac = pygame.Surface((u, u))
+pac.set_colorkey(black)
+pygame.draw.circle(pac, yellow, (u/2, u/2), u/2)
 
-# end program when quit
+pac_rect = pac.get_rect()
+pac_rect.move_ip((14 * u, 23 * u))      # Set starting pos
+
+# Program definitions
+direction = (-2, 0)
+
+clock = pygame.time.Clock()
+
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.KEYDOWN:
+            direction = {pygame.K_LEFT: (-2, 0), pygame.K_UP: (0, -2),
+                         pygame.K_RIGHT: (2, 0), pygame.K_DOWN: (0, 2)}.get(event.key, direction)
+        elif event.type == pygame.QUIT:
             sys.exit()
+
+    pac_rect.move_ip(direction)
+
+    screen.blit(background, (0, 0))
+    screen.blit(pac, pac_rect)
+
+    clock.tick(60)
+    pygame.display.flip()
