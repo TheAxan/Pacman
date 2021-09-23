c4f75e9e127711c793ca6125da95b086112c4c4f
TheAxan <88592270+TheAxan@users.noreply.github.com>
2021-09-23 10:39:02 -0400

    reformated into methods, little refactoring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-from math import floor, ceil
 
 pygame.init()
 
@@ -19,9 +18,9 @@ map_grid = (
     (0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0),
     (0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0),
     (0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0),
-    (1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1),
+    (1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1),
     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    (1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1),
+    (1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1),
     (0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0),
     (0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0),
     (0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0),
@@ -39,6 +38,8 @@ map_grid = (
     (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 )
 
+
+clock = pygame.time.Clock()
 u = 38      # 1 unit is 30 pixels by default (840x930), 38 for 1064x1178, 34 for 952x1054
 screen = pygame.display.set_mode((28 * u, 31 * u))
 
@@ -48,6 +49,10 @@ black = 0, 0, 0
 dark_grey = 15, 15, 15
 yellow = 255, 255, 0
 blue = 0, 0, 255
+red = 255, 0, 0
+orange = 255, 153, 0
+cyan = 0, 230, 230
+pink = 255, 179, 255
 
 # background surface
 background = pygame.Surface((28 * u, 31 * u))
@@ -67,7 +72,7 @@ for row in map_grid:
 
 
 class entity:
-    def __init__(self, x, y, speed_divider, direction) -> None:
+    def __init__(self, x, y, speed_divider, direction, color) -> None:
         self.surface = pygame.Surface((u, u))
         self.surface.set_colorkey(black)
         self.rect = self.surface.get_rect()
@@ -83,67 +88,70 @@ class entity:
             'down': (0, self.speed),
         }[direction]
 
-pac = entity(14, 23, 15, 'left')
-pygame.draw.circle(pac.surface, yellow, (u/2, u/2), u/2)
+    
+    def update_pos(self):
+        self.x = round(self.rect.x / u)
+        self.y = round(self.rect.y / u)
 
-# Program definitions
-direction_input = None
-clock = pygame.time.Clock()
+    def tunnel(self):
+        if self.y == 14:
+            if self.x == -1:
+                self.rect.x = 28 * u
+            elif self.x == 28:
+                self.rect.x = -1 * u
+
+class player(entity):
+    def __init__(self, x, y, speed_divider, direction, color) -> None:
+        super().__init__(x, y, speed_divider, direction, color)
+        self.input = None
+        pygame.draw.circle(self.surface, color, (u/2, u/2), u/2)
+    
+    def update_movement(self):
+        if self.input is not None:    
+            if (map_grid[self.y + int(self.input[1] / self.speed)]
+                        [self.x + int(self.input[0] / self.speed)]) == 0:
+                self.movement = self.input
+                self.input = None
+            else:
+                self.input = None
+    
+    def wall_stop(self):
+        if (map_grid[self.y + int(self.movement[1] / self.speed)]
+                    [self.x + int(self.movement[0] / self.speed)]):
+            self.movement = (0, 0)
+            self.rect.x = round(self.rect.x / u) * u
+            self.rect.y = round(self.rect.y / u) * u
+            self.x = int(self.rect.x / u)
+            self.y = int(self.rect.y / u)
+
+pac = player(14, 23, 15, 'left', yellow)
 
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key in (pygame.K_LEFT, pygame.K_UP,
-                                                          pygame.K_RIGHT, pygame.K_DOWN):
-            direction_input = {
-                
-                # Probably want to reorganize this
-                
-                pygame.K_LEFT: (-pac.speed, 0),
-                pygame.K_UP: (0, -pac.speed),
-                pygame.K_RIGHT: (pac.speed, 0),
-                pygame.K_DOWN: (0, pac.speed),
-            }[event.key]
+        if event.type == pygame.KEYDOWN:
+            if event.key in (pygame.K_LEFT, pygame.K_UP, pygame.K_RIGHT, pygame.K_DOWN):
+                pac.input = {
+                    pygame.K_LEFT: (-pac.speed, 0),
+                    pygame.K_UP: (0, -pac.speed),
+                    pygame.K_RIGHT: (pac.speed, 0),
+                    pygame.K_DOWN: (0, pac.speed),
+                }[event.key]
+            elif event.key in ():
+                pass
+        
         elif event.type == pygame.QUIT:
             sys.exit()
 
-    # change movement to direction input on full squares
-    if direction_input is not None and pac.x == pac.rect.x / u and pac.y == pac.rect.y / u:
-        if (map_grid[int(pac.y + direction_input[1] / pac.speed)]
-                    [int(pac.x + direction_input[0] / pac.speed)]) == 0:
-            pac.movement = direction_input
-            direction_input = None
-        else:
-            direction_input = None
+    
+    if pac.x == pac.rect.x / u and pac.y == pac.rect.y / u:     # on full squares
+        pac.update_movement()
+        pac.tunnel()
+        pac.wall_stop()
 
-    # Move pac before cell update and wall-check to prevent wall ramming
+
     pac.rect.move_ip(pac.movement)
-
-    # Update cell position
-    if pac.movement[0] == -pac.speed:
-        pac.x = floor(pac.rect.x / u)
-    elif pac.movement[0] == pac.speed:
-        pac.x = ceil(pac.rect.x / u)
-
-    elif pac.movement[1] == -pac.speed:
-        pac.y = floor(pac.rect.y / u)
-    elif pac.movement[1] == pac.speed:
-        pac.y = ceil(pac.rect.y / u)
-
-    # Immobilizes and resets pos if cell ahead is a wall
-    if map_grid[pac.y][pac.x]:
-        pac.movement = (0, 0)
-        pac.rect.x = round(pac.rect.x / u) * u
-        pac.rect.y = round(pac.rect.y / u) * u
-        pac.x = int(pac.rect.x / u)
-        pac.y = int(pac.rect.y / u)
-
-    # Tunnel
-    if pac.y == 14:
-        if pac.x == -2:
-            pac.rect.x = 29 * u
-        elif pac.x == 29:
-            pac.rect.x = -2 * u
+    pac.update_pos()
 
     # Refresh screen
     screen.blit(background, (0, 0))
