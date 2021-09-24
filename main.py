21fd8237e050f92ec27c1df9f92cfc591d400c64
TheAxan <88592270+TheAxan@users.noreply.github.com>
2021-09-23 20:08:13 -0400

    Added unit definition according to resolution

--- main.py
+++ main.py
@@ -40,7 +40,12 @@ map_grid = (
 
 
 clock = pygame.time.Clock()
-u = 38      # 1 unit is 30 pixels by default (840x930), 38 for 1064x1178, 34 for 952x1054
+
+pygame.display.set_mode()  # The display must be initialized first or it might get the wrong res
+u = {  # 1 unit is 30 pixels by default (840x930), 38 for 1064x1178, 34 for 952x1054
+    (1920, 1080): 34,
+    (1080, 1920): 38,
+}.get((pygame.display.Info().current_w, pygame.display.Info().current_h), 30)
 screen = pygame.display.set_mode((28 * u, 31 * u))
 
 # colors definition
