ddd58b948e7fb7c94ea0e988dea3b416ce82ba44
TheAxan <axlevert@gmail.com>
2021-10-02 09:14:05 -0400

    added 1440p

--- main.py
+++ main.py
@@ -45,8 +45,9 @@ pygame.display.set_mode()  # The display must be initialized first or it might g
 u = {  # 1 unit is 30 pixels by default (840x930), 38 for 1064x1178, 34 for 952x1054
     (1920, 1080): 34,
     (1080, 1920): 38,
+    (2560, 1440): 44,  # 46 messes with wall_stop and 45 with corner??
 }.get((pygame.display.Info().current_w, pygame.display.Info().current_h), 30)
-screen = pygame.display.set_mode((28 * u, 31 * u))
+screen = pygame.display.set_mode((28 * u, 31 * u), flags=pygame.NOFRAME)
 
 
 # colors definition
