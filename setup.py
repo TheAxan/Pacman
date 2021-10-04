74fa812f9682af1e0c1736ca3129fcb189943d91
TheAxan <88592270+TheAxan@users.noreply.github.com>
2021-10-04 18:27:43 -0400

    Explored a bug, added explanatory comment
    an odd u value would break some functions

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import pygame
 
 pygame.display.set_mode()  # The display must be initialized first or it might get the wrong res
 u = {  # cells are u lenght squares
+    # u must be even or the graphics looks off and wall_stop breaks 
+    # because full_cell_check breaks because the resulting speed jumps over full cells.
+    # Trying to account for the jump is ressource intensive since the method runs every frame
     (1920, 1080): 34,
     (1080, 1920): 38,
-    (2560, 1440): 44,  # 46 messes with wall_stop and 45 with corner??
+    (2560, 1440): 44,
 }.get((pygame.display.Info().current_w, pygame.display.Info().current_h), 30)
 screen = pygame.display.set_mode((28 * u, 31 * u), flags=pygame.NOFRAME)
 
