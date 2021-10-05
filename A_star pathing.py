b2172d21e7dc2db745602da2c430ebcc08179ebf
TheAxan <88592270+TheAxan@users.noreply.github.com>
2021-10-05 07:04:30 -0400

    Start of A* pathing

---
+++ A_star pathing.py
@@ -0,0 +1,74 @@
+import pygame
+import random
+from sys import exit
+
+pygame.init()
+
+u = 30
+
+array = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+while True:
+    for row in array:
+        for _ in range(random.randrange(10)):
+            row[random.randrange(20)] = 1
+
+
+    white = 255, 255, 255
+    orange = 255, 153, 0
+    cyan = 0, 230, 230
+    black = 0,0,0
+    transparent = 0,0,0,0
+
+    screen = pygame.display.set_mode((20*u, 20*u), flags=pygame.NOFRAME)
+    background = pygame.Surface((20*u,20*u))
+
+
+    outer_square = pygame.Surface((u, u))
+    outer_square.fill(white)
+    pygame.draw.rect(outer_square, black, (0, 0, u, u), 2)
+
+    black_square = pygame.Surface((u, u))
+    black_square.fill(black)
+
+    for y_counter, row in enumerate(array):
+        for x_counter, cell in enumerate(row):
+            background.blit(outer_square, (x_counter * u, y_counter * u))
+            if array[y_counter][x_counter] == 1:
+                background.blit(black_square, (x_counter * u, y_counter * u))
+
+    screen.blit(background, (0,0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key is pygame.K_ESCAPE:
+                    exit()
+                if event.key is pygame.K_BACKSPACE:
+                    pass
+                    # TODO break from nested loop (nearest while true)
+            elif event.type == pygame.QUIT:
+                exit()
+        
+        pygame.display.flip()
