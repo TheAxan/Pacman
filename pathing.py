952cff46fc9114fdcb91cb258a31cc6d6a5d0234
TheAxan <88592270+TheAxan@users.noreply.github.com>
2021-10-06 01:57:51 -0400

    Played around with uniform cost search
    https://www.redblobgames.com/pathfinding/a-star/introduction.html

--- pathing.py
+++ pathing.py
@@ -1,20 +1,44 @@
 import queue
-from pathingDisplay import array, origin
+import pygame
+from pathingDisplay import *
 
 
-class Point():
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
+node_array = tools.create_empty_array(len(array[0]), len(array))
 
-def neighbors():
-    pass
+def neighbors(center):
+    neighbors_list = []
+    for i, j in ((-1,0), (0,-1), (1,0), (0,1)):
+        try:
+            if center[0] + i < 0 or center[1] + j < 0:
+                raise Exception('negative index')
+            if array[center[1] + j][center[0] + i] == 0 or array[center[1] + j][center[0]] == 3:
+                neighbors_list.append((center[0] + i, center[1] + j))
+        except:
+            pass
+    return neighbors_list
 
+frontier = queue.PriorityQueue()
+frontier.put([0, origin])
+came_from = dict()
+cost_so_far = dict()
+came_from[origin] = None
+cost_so_far[origin] = 0
 
-frontier = queue.Queue()
-frontier.put(origin)
-reached = set()
-reached.add(origin)
-
-while not frontier.empty:
+while not frontier.empty():
     current = frontier.get()
+    if current[1] == end:
+        break
+    for next in neighbors(current[1]):
+        new_cost = cost_so_far[current[1]] + 1
+        if next not in cost_so_far or new_cost < cost_so_far[next]:
+            cost_so_far[next] = new_cost
+            frontier.put([new_cost, next])
+            came_from[next] = current[1]
+            screen.blit(orange_square, (next[0]*u, next[1]*u))
+
+current[1] = end
+while current[1] != origin:
+    screen.blit(cyan_square, (current[1][0]*u, current[1][1]*u))
+    current[1] = came_from[current[1]]
+    pygame.display.flip()
+print(input())
