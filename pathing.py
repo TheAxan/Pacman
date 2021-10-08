d73431c613f63a55e57b6a65d8bee79cb2d4a4b0
TheAxan <88592270+TheAxan@users.noreply.github.com>
2021-10-07 22:44:56 -0400

    renammed variables and function

--- pathing.py
+++ pathing.py
@@ -1,47 +1,49 @@
 import queue
 import pygame
 from pathingDisplay import *
+import pathingDisplay
 
+start_node = pathingDisplay.origin
+end_node = pathingDisplay.end
 
-
-
-def neighbors(center_point, array, accepted_neighbor_values):
+def neighbors(center_node, array, accepted_neighbor_values):
     neighbors_set = set()
     for i, j in ((-1,0), (0,-1), (1,0), (0,1)):
         try:
-            if center_point[0] + i < 0 or center_point[1] + j < 0:
+            if center_node[0] + i < 0 or center_node[1] + j < 0:
                 raise Exception('negative index')
-            if array[center_point[1] + j][center_point[0] + i] in accepted_neighbor_values:
-                neighbors_set.add((center_point[0] + i, center_point[1] + j))
+            if array[center_node[1] + j][center_node[0] + i] in accepted_neighbor_values:
+                neighbors_set.add((center_node[0] + i, center_node[1] + j))
         except:
             pass
     return neighbors_set
 
-def heuristic(a, b):
+def heuristic_cost(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-frontier = queue.PriorityQueue()
-frontier.put([0, origin])
-came_from = dict()
-cost_so_far = dict()
-came_from[origin] = None
-cost_so_far[origin] = 0
+nodes_to_explore = queue.PriorityQueue()
+origin_node = dict()
+g_cost = dict()  # g cost: # of moves from the start_node
 
-while not frontier.empty():
-    current = frontier.get()
-    if current[1] == end:
+nodes_to_explore.put([0, start_node])
+origin_node[start_node] = None
+g_cost[start_node] = 0
+
+while not nodes_to_explore.empty():
+    current_node = nodes_to_explore.get()
+    if current_node[1] == end_node:
         break
-    for next in neighbors(current[1], random_array, (0, 3)):
-        new_cost = cost_so_far[current[1]] + 1
-        if next not in cost_so_far or new_cost < cost_so_far[next]:
-            cost_so_far[next] = new_cost
-            frontier.put([new_cost + heuristic(end, next), next])
-            came_from[next] = current[1]
-            screen.blit(orange_square, (next[0]*u, next[1]*u))
+    for new_node in neighbors(current_node[1], random_array, (0, 3)):
+        new_g_cost = g_cost[current_node[1]] + 1
+        if new_node not in g_cost or new_g_cost < g_cost[new_node]:
+            g_cost[new_node] = new_g_cost
+            nodes_to_explore.put([new_g_cost + heuristic_cost(end_node, new_node), new_node])
+            origin_node[new_node] = current_node[1]
+            screen.blit(orange_square, (new_node[0]*u, new_node[1]*u))
 
-current[1] = end
-while current[1] != origin:
-    screen.blit(cyan_square, (current[1][0]*u, current[1][1]*u))
-    current[1] = came_from[current[1]]
+current_node[1] = end_node
+while current_node[1] != start_node:
+    screen.blit(cyan_square, (current_node[1][0]*u, current_node[1][1]*u))
+    current_node[1] = origin_node[current_node[1]]
     pygame.display.flip()
 print(input())
