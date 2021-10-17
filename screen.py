39095b9f4c7d08e308a289f84da2e744c7392c92
TheAxan <88592270+TheAxan@users.noreply.github.com>
2021-10-17 09:20:17 -0400

    simplified screen module , reduced walls size

--- screen.py
+++ screen.py
@@ -1,20 +1,13 @@
 import pygame
 from maps import default_map as map_grid
 
-pygame.display.set_mode()  # The display must be initialized first or it might get the wrong res
-cu: int = {
-    # cells are u lenght squares
-    # u must be even or the graphics looks off and wall_stop breaks
-    # * additionnal note: it doesn't work if u <= 16 BUG and the speed is all messed up
-    # because full_cell_check breaks because the resulting speed jumps over full cells.
-    # Trying to account for the jump is ressource intensive since the method runs every frame
-    (1920, 1080): 34,
-    (1080, 1920): 38,
-    (2560, 1440): 44,
-}.get((pygame.display.Info().current_w, pygame.display.Info().current_h), 30)
-screen = pygame.display.set_mode((28 * cu, 31 * cu), flags=pygame.NOFRAME)
 
-gu = cu * 1.5
+map_width = len(map_grid[0])
+map_height = len(map_grid)
+
+cu = 16 # TODO switch to 8 so as to scale the screen only and remove sprite scaling, NOTE: this requires a speed/move_ip system overhaul due to int rounding affecting speed and immobilizing at 8
+screen = pygame.display.set_mode((map_width * cu, map_height * cu), pygame.SCALED)
+gu = cu * 2
 # gu is short for graphical_unit, cu for cell_unit
 # the long names would make most functions absurdly long (especially for gu drawings)
 
@@ -28,11 +21,11 @@ orange = 255, 153, 0
 cyan = 0, 230, 230
 pink = 255, 179, 255
 
-background = pygame.Surface((28 * cu, 31 * cu))
+background = pygame.Surface((map_width * cu, map_height * cu))
 
 square = pygame.Surface((cu, cu))
 square.fill(blue)
-pygame.draw.rect(square, black, (0, 0, cu, cu), int(gu/3))
+pygame.draw.rect(square, black, (0, 0, cu, cu), int(gu/2.5))
 
 for y_counter, row in enumerate(map_grid):
     for x_counter, cell in enumerate(row):
